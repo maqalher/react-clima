96637497173e816e40e57c771a636d0428140fc6
maqalher <maqalher@hotmail.com>
2020-10-23 19:34:16 -0500

    add clima project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
+
+  // state del formulario
+  const [ busqueda, guardarBusqueda ] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [ consultar, guardarConsultar ] = useState(false);
+  const [ resultado, guardarResultado ] = useState({});
+  const [ error, guardarError ] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    //console.log(ciudad);
+
+    const consultarAPI = async () => {
+
+      if(consultar){
+        // api http://api.openweathermap.org/data/2.5/weather?q=pachuca,mexico&appid=7db9bef23b5d07786116aaa5abfec91d
+        const appId = '7db9bef23b5d07786116aaa5abfec91d';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+  
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        
+        guardarResultado(resultado);
+        guardarConsultar(false);
+
+        // Detecta si hubo resultados correctos en la consulta
+
+        if(resultado.cod === '404'){
+          guardarError(true);
+        }else{
+          guardarError(false);
+        }
+
+      } 
+    }
+
+    consultarAPI();
+    // eslint-disable-next-line 
+  },[consultar]);
+
+  let componente;
+  if(error){
+    componente = <Error mensaje="No hay resultados" />
+  }else{
+    componente = <Clima resultado={resultado} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header
+        titulo='Clima React App'
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+              <div className="col m6 s12">
+                <Formulario 
+                  busqueda={busqueda}
+                  guardarBusqueda={guardarBusqueda}
+                  guardarConsultar={guardarConsultar}
+                />
+              </div>
+              <div className="col m6 s12">
+                {componente}
+              </div>
+          </div>
+        </div>
+      </div>
+
+    </Fragment>
   );
 }
 
